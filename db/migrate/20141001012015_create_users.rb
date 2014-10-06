0d66c6ce1a962784aa6d4cf95f7ec35ce89c106f
frostyfire03530 <frostyfire03530@gmail.com>
2014-10-06 23:24:32 +0000

    Fixed some things in the database for how users are created through devise.

--- db/migrate/20141001012015_create_users.rb
+++ db/migrate/20141001012015_create_users.rb
@@ -1,11 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email
-      t.string :password
+      t.string :first_name
+      t.string :last_name
       t.decimal :payrate
-
+      t.string :role
+      t.boolean :active
+      
       t.timestamps
     end
   end
