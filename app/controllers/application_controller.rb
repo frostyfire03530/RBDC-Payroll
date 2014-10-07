7d7d9a0c6d2ceffb2d3fb0bfc3af455a9fe02bbb
frostyfire03530 <frostyfire03530@gmail.com>
2014-10-07 07:55:33 +0000

    WIP: when a user updates their profile on their own, it doesn't redirect right.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def after_sign_in_path_for(resource)
-      if user_signed_in? && (current_user.role == 'user')
-        user_timesheets_path(current_user.id)
-      elsif user_signed_in? && (current_user.role == 'admin')
-        users_path
-      end
+    if user_signed_in? && (current_user.role == 'user')
+      user_timesheets_path(current_user.id)
+    elsif user_signed_in? && (current_user.role == 'admin')
+      users_path
+    end
+  end
+  
+  protected
+  
+  def after_update_path_for(resource)
+      user_path(current_user.id)
   end
 end
