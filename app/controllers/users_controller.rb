8ec504f09a9cb9a84bba10488601184593bedaf1
frostyfire03530 <frostyfire03530@gmail.com>
2014-10-07 07:56:45 +0000

    Moved some code in the users_controller into functions so it wasn't so tightly coupled.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,54 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  if defined? current_user
-    if current_user.role != :admin
-      redirect_to user_timesheets_path(current_user.id), notice: 'You do not have access to this resource.'
+
+  def admin_check
+    if defined?(current_user.id)
+      if current_user.role != 'admin'
+        redirect_to user_timesheets_path(current_user.id), notice: 'You do not have access to this resource.'
+      end
+    else
+      redirect_to new_user_session_path
+    end
+  end
+
+  def initial_check
+    if defined?(current_user.id)
+      if current_user.role != 'admin'
+        @url_user = User.find(params[:id])
+        if current_user.id != @url_user.id
+          redirect_to user_timesheets_path(current_user.id), notice: 'You do not have access to this resource.'
+        end
+      end
+    else
+      redirect_to new_user_session_path
     end
   end
 
   # GET /users
   def index
+    admin_check
     @users = User.all
   end
 
   # GET /users/1
   def show
+    initial_check
   end
 
   # GET /users/new
   def new
+    admin_check
     @user = User.new
   end
 
   # GET /users/1/edit
   def edit
+    initial_check
   end
 
   # POST /users
   def create
+    admin_check
     @user = User.new(user_params)
     @user.encrypted_password = user_params['password'].encrypted_password
 
@@ -38,6 +61,7 @@ class UsersController < ApplicationController
 
   # PATCH/PUT /users/1
   def update
+    initial_check
     if @user.update(user_params)
       redirect_to @user, notice: 'User was successfully updated.'
     else
@@ -47,6 +71,7 @@ class UsersController < ApplicationController
 
   # DELETE /users/1
   def destroy
+    initial_check
     @user.destroy
     redirect_to users_url, notice: 'User was successfully destroyed.'
   end
