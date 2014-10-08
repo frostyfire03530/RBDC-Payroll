4f665938f64fc027b58e69a6d2ad78b1c9395034
frostyfire03530 <frostyfire03530@gmail.com>
2014-10-08 21:23:29 +0000

    Added some of the models into instance variables to be used on the acconting index.

--- app/controllers/accounting_controller.rb
+++ app/controllers/accounting_controller.rb
@@ -6,9 +6,35 @@ class AccountingController < ApplicationController
 
   # GET /accounting
   def index
+      @projects = Project.all
+      @users_active = User.where(active: true)
+      @users_inactive = User.where(active: false)
     #earliest_date = Model.first(:order => 'column asc')
-      @results = Timesheet.pluck(:date).sort.first
-     #@results = ActiveRecord::Base.connection.select_all(query)
+      @oldest_date_in_timesheet = Timesheet.pluck(:date).sort.first
+      
+      is_first_part_of_month = false
+      if @oldest_date_in_timesheet.strftime("%d").to_i <= 15
+        is_first_part_of_month = true
+      end
+      
+      @payperiods = []
+      
+      if is_first_part_of_month
+        @payperiods << @oldest_date_in_timesheet.strftime("%m/01/%Y -> %m/15/%Y")
+      else
+        @payperiods << @oldest_date_in_timesheet.end_of_month.strftime("%m/16/%Y -> %m/%d/%Y")
+      end
+      
+      #if is_first_part_of_month
+      #  @timesheets = Timesheet.where("date >= :start_date AND date <= :end_date", {start_date: @oldest_date_in_timesheet.at_beginning_of_month, end_date: (@oldest_date_in_timesheet.at_beginning_of_month + 14)}.order("date ASC")
+      #else
+      #  @timesheets = Timesheet.where("date >= :start_date AND date <= :end_date AND user_id = :url_user", {start_date: (Date.today.at_beginning_of_month + 15), end_date: (Date.today.end_of_month), url_user: @url_user.id}).order("date ASC")
+      #end
+          
+
+      
+      
+      #@results = ActiveRecord::Base.connection.select_all(query)
   end
 
 
