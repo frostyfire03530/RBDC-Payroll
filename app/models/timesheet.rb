aef1f88e808a16a6429d0e08ed37ddafc59933fb
frostyfire03530 <frostyfire03530@gmail.com>
2014-10-11 00:22:11 +0000

    Added new validation method to timesheet model. Also included the seed file.

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -2,15 +2,17 @@ class Timesheet < ActiveRecord::Base
     belongs_to :user
 
     validates :date, :timein, :timeout, :hours, :description, :payrate, :project, presence: true
-    validate :timesheet_cannot_be_in_the_future, :timeout_cannot_be_before_timein, :date_is_not_in_pay_period
+    validate :timesheet_cannot_be_in_the_future, :timeout_cannot_be_before_timein, :date_is_not_in_pay_period, :timesheets_cannot_overlap
     
     def timesheet_cannot_be_in_the_future
+        #Check if the user has inputed a timeout that is after the current time and if true create an error.
         if timeout.present? && timeout >= Time.now
-            return errors.add(:timeout, "cannot be for a future time")
+            errors.add(:timeout, "cannot be for a future time")
         end
         
+        #Check if the user has inputed a date that is after the current date and if true create an error.
         if date.present? && date > Date.today
-            return errors.add(:date, "cannot be for a future date")
+            errors.add(:date, "cannot be for a future date")
         end
     end
     
@@ -31,14 +33,33 @@ class Timesheet < ActiveRecord::Base
             is_same_month_and_year = true
         end
         if !is_same_month_and_year
-            return errors.add(:date, "is not in pay period")
-        end
-        if (is_same_month_and_year && is_first_part_of_month) && date.strftime("%d").to_i >= 16
-            return errors.add(:date, "is not in pay period")
-        end
-        if (is_same_month_and_year && !is_first_part_of_month) && date.strftime("%d").to_i <= 15
-            return errors.add(:date, "is not in pay period")
+            errors.add(:date, "is not in pay period")
+        elsif (is_same_month_and_year && is_first_part_of_month) && date.strftime("%d").to_i >= 16
+            errors.add(:date, "is not in pay period")
+        elsif (is_same_month_and_year && !is_first_part_of_month) && date.strftime("%d").to_i <= 15
+            errors.add(:date, "is not in pay period")
         end
     end
-        
+    
+    def timesheets_cannot_overlap
+        #Grab all the timesheets for the current_user
+        user_timesheets = Timesheet.where(user_id: @url_user.id)
+    
+        #Loop through each of the current_user's timesheets
+        user_timesheets.each do | user_timesheet |
+      
+            #Check to see if the timein / timeout is between the current timesheet, if true create an error and break out of the loop.
+            if timein > user_timesheet.timein && timein < user_timesheet.timeout
+                errors.add(:timein, "cannot overlap with previous timesheet.")
+                errors.add(:timeout, "cannot overlap with previous timesheet.")
+                break
+            end
+            if timeout > user_timesheet.timein && timeout < user_timesheet.timeout
+                errors.add(:timein, "cannot overlap with previous timesheet.")
+                errors.add(:timeout, "cannot overlap with previous timesheet.")
+                break
+            end
+        end    
+    end
+    
 end
